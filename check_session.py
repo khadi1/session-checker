89b72e57c38f101f73d27263438696cfef812712
khadi1 <jaouakhadija@gmail.com>
2025-05-02 16:42:25 +0100

    adding the last object

--- check_session.py
+++ check_session.py
@@ -3,7 +3,7 @@ import os
 import smtplib
 from datetime import datetime,  timedelta
 import time 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 
 
@@ -11,7 +11,7 @@ import time
 
 
 
-# load_dotenv() 
+load_dotenv() 
 APP_KEY = os.getenv("APP_KEY") 
 FROM_EMAIL = os.getenv("FROM_EMAIL") 
 TO_EMAIL = os.getenv("FROM_EMAIL") 
@@ -21,11 +21,11 @@ URL_SESSION = os.getenv("URL_SESSION")
 
 
 
-def send_email(subject, body):
+def send_email(subject, body, timestamp):
 
     
     
-    content = f"Subject: {subject}\n\n{body}"
+    content = f"Subject: {subject}\n\n{timestamp}{body}"
 
     server = smtplib.SMTP("smtp.gmail.com" , 587)
     server.starttls()
@@ -43,9 +43,20 @@ def send_email(subject, body):
 
 
 
+PAYLOAD = os.getenv("PAYLOAD")
+
+def create_the_post_request(data):
+    payload =  json.loads(PAYLOAD)
+    payload["session"] = data
+    return payload
+
+
+
+
 import requests
 import json
 from datetime import datetime
+
 
 
 
@@ -56,14 +67,15 @@ def check_and_save():
     if response.status_code != 404:
         data = response.json()
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        data = create_the_post_request(data)
         filename = f"session_37_{timestamp}.json"
 
         with open(filename, "w") as f:
             json.dump(data, f, indent=2)
 
-        print(f"Session available and saved to {filename}")
+        # print(data)
 
-        send_email("Boosteno" , data)
+        send_email("Boosteno" , data, timestamp)
     else:
         print("Session not available (404)")
 
@@ -84,6 +96,7 @@ while datetime.now() < end_time:
     
     print(f" Waiting for 30 seconds... Current time: {datetime.now()}", flush=True)
     time.sleep(30)
+
 
 
 
