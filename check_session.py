f3a970675c06f2294944e2bc32bedf300147bf2d
khadi1 <jaouakhadija@gmail.com>
2025-05-02 17:59:07 +0100

    add doocu

--- check_session.py
+++ check_session.py
@@ -11,7 +11,7 @@ if os.getenv("ENV") != "production":
     from dotenv import load_dotenv
     load_dotenv()
 
- 
+
 APP_KEY = os.getenv("APP_KEY") 
 FROM_EMAIL = os.getenv("FROM_EMAIL") 
 TO_EMAIL = os.getenv("FROM_EMAIL") 
@@ -42,6 +42,29 @@ def send_email(subject, body, timestamp):
 
 
 
+import gnupg
+
+
+def image_decrypt():
+# Initialize GPG
+    gpg = gnupg.GPG()
+
+    # Open the encrypted file and decrypt it
+    with open('Doc.pdf.gpg', 'rb') as f:
+        decrypted_data = gpg.decrypt_file(f, passphrase=os.getenv("PASSPHRASE"))
+        
+    if decrypted_data.ok:
+        with open('Doc.pdf', 'wb') as out:
+            out.write(decrypted_data.data)
+        print("Decryption successful")
+    else:
+        print("Decryption failed:", decrypted_data.status)
+
+
+
+
+
+
 
 PAYLOAD = os.getenv("PAYLOAD")
 
@@ -51,6 +74,37 @@ def create_the_post_request(data):
     return payload
 
 
+
+def add_document(data):
+    image_decrypt()
+    file_path = "Doc.pdf"
+    url_doc = os.getenv("URL_DOC")
+    
+
+    files = {'file': open(file_path, 'rb')}
+
+    response = requests.post(url_doc,  files=files)
+
+    files['file'].close()
+
+    if response.status_code == 200:
+        response_data = response.json()
+        
+        if response_data.get('message') == 'success':
+            file_name = response_data.get('fileName')
+            
+            document = {
+                "id": None,
+                "documentType": "PASSPORT",
+                "url": file_name,
+                "verifiedByApi": False
+            }
+            
+            data["document"] = document
+            return data
+    else:
+        print(f"Upload failed with status code: {response.status_code}")
+        print(response.text)
 
 
 import requests
@@ -67,6 +121,7 @@ def check_and_save():
     if response.status_code != 404:
         data = response.json()
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        data = add_document(data)
         data = create_the_post_request(data)
         filename = f"session_37_{timestamp}.json"
 
